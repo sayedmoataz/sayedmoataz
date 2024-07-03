bad7033b16d6569660e08d1909e92aeebe68dbab
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2024-07-03 22:14:42 +0300

    Update console.js

--- console.js
+++ console.js
@@ -3,3 +3,10 @@ console.log("hello") //this is second
 console.log("hello") //this is second
 console.log("hello") //this is second
 console.log("hello") //this is third
+
+
+
+function main() {
+  return null; }
+main()
+//console.log(main)
